147f9d71f125778e332ef97adb397a2dce299024
jeanheberthsouzavieira <jeanheberth19@gmail.com>
2025-02-13 16:40:58 -0300

    adicionados classes de security

--- src/test/java/br/com/usuario/UsuarioApplicationTests.java
+++ src/test/java/br/com/usuario/UsuarioApplicationTests.java
@@ -1,13 +1,13 @@
-package br.com.usuario;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class UsuarioApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package br.com.usuario;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class UsuarioApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
