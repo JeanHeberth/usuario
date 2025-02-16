ad4e0c2d169fab7ba9b5e88e6c22f683e5aa4d04
jeanheberth <jeanheberth19@gmail.com>
2025-02-16 15:56:32 -0300

    Criando dto e realizando a conversao.

---
+++ src/main/java/br/com/usuario/controller/UsuarioController.java
@@ -0,0 +1,23 @@
+package br.com.usuario.controller;
+
+import br.com.usuario.business.UsuarioService;
+import br.com.usuario.business.dto.UsuarioDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuarios")
+@RequiredArgsConstructor
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<UsuarioDTO> salvar(@RequestBody UsuarioDTO usuarioDTO) {
+        return ResponseEntity.ok().body(usuarioService.salvar(usuarioDTO));
+    }
+}
